cdc220967c7e66f2012afd295770bbe9c64391c8
s11110101111 <s11110101111@gmail.com>
2020-11-28 09:01:45 +0500

    1:38:43 watch hand

--- src/main/java/org/example/ApplicationContext.java
+++ src/main/java/org/example/ApplicationContext.java
@@ -1,12 +1,31 @@
 package org.example;
 
+import lombok.Getter;
+
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
-public class ApplicationContext {
+public class  ApplicationContext {
     private ObjectFactory objectFactory;
+    @Getter
+    private Config config;
     private Map<Class, Object> cache = new ConcurrentHashMap<>();
+
     public <T> T getObject(Class<T> type){
-        return null;
+        Class<? extends T> implClass = type;
+    if (cache.containsKey(type)){
+        return (T) cache.get(type);
+    }
+
+        if (type.isInterface()) {
+            implClass = config.getImplClass(type);
+        }
+        T t  = objectFactory.createObject(implClass);
+
+        if (implClass.isAnnotationPresent(Singleton.class)){
+            cache.put(type,t);
+
+        }
+        return t;
     }
 }
--- src/main/java/org/example/ObjectFactory.java
+++ src/main/java/org/example/ObjectFactory.java
@@ -1,48 +1,35 @@
 package org.example;
 
 import lombok.SneakyThrows;
-
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.ObjectInputFilter;
-import java.lang.invoke.SerializedLambda;
-import java.lang.reflect.Field;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
-import java.util.stream.Stream;
-
-import static java.util.stream.Collectors.toMap;
-
 
 public class ObjectFactory {
     private static final ObjectFactory ourInstance = new ObjectFactory();
     private List<ObjectConfigurator> configurators = new ArrayList<>();
     private Config config;
+    private final ApplicationContext context;
 
     public static ObjectFactory getInstance() {
         return ourInstance;
     }
-@SneakyThrows
-    private ObjectFactory() {
-        config = new JavaConfig("org.example", new HashMap<>(Map.of(Policeman.class, AngryPolicemanImpl.class)));
-        for (Class<? extends ObjectConfigurator> aClass : config.getScanner().getSubTypesOf(ObjectConfigurator.class)) {
+
+    @SneakyThrows
+    public ObjectFactory(ApplicationContext context) {
+        this.context = context;
+        for (Class<? extends ObjectConfigurator> aClass : context.getConfig().getScanner().getSubTypesOf(ObjectConfigurator.class)) {
             configurators.add(aClass.getDeclaredConstructor().newInstance());
 
         }
     }
 
     @SneakyThrows
-    public <T> T createObject(Class<T> type) {
-        Class<? extends T> implClass = type;
-        if (type.isInterface()) {
-            implClass = config.getImplClass(type);
-        }
+    public <T> T createObject(Class<T> implClass) {
+
         T t = implClass.getDeclaredConstructor().newInstance();
 
         //todo setting object t is  here
-            configurators.forEach(objectConfigurator -> objectConfigurator.configure(t));
+        configurators.forEach(objectConfigurator -> objectConfigurator.configure(t, context));
 
 
         return t;
